6e62e7e544fad1da8dbc80496c94e9a880b7f90b
Emerson <eemr3@yahoo.com.br>
2021-12-14 15:42:10 -0300

    requisito 03 em andamento

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Routes from './Routes/Routes';
 
 function App() {
   return (
-    <div className="App"> </div>
+    <div className="App">
+      <Routes />
+    </div>
   );
 }
 
