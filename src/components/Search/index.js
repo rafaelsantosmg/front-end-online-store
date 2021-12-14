48acc4295e5977f907e6e5baf299f53b58fcc149
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-12-14 15:24:49 -0300

    feat: implementado o component Header e Search

---
+++ src/components/Search/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
+class Search extends Component {
+  render() {
+    return (
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <Button variant="outline-secondary" id="button-addon2">
+          
+        </Button>
+      </InputGroup>
+    );
+  }
+}
+
+export default Search;
