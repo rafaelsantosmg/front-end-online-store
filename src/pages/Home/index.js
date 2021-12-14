0848f87580159b681502df10f1ab1a2cc4b8a03f
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-12-14 17:58:15 -0300

    feat(Primeira fase): Iniciado a Implementação do component categories

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Categories from '../../components/Categories';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        Home
+      <div className="home-page">
+        <Categories />
       </div>
     );
   }
