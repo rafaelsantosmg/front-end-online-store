82a1c48e5f26d1baf2881d0db4f8dac49c25bec8
Emerson <eemr3@yahoo.com.br>
2021-12-17 13:41:58 -0300

    fix: app e shoppingCart

--- src/App.js
+++ src/App.js
@@ -33,28 +33,29 @@ class App extends Component {
         return acc.concat(product);
       }, []),
     }));
+  }
 
-//   addProductCart = (idProduct) => {
-//     const { products, cartProduct } = this.state;
-//     let sameProduct = false;
-//     cartProduct.forEach((product) => {
-//       if (product.id === idProduct) {
-//         if (product.quantity) {
-//           product.quantity += 1;
-//         } else {
-//           product.quantity = 2;
-//         }
-//         sameProduct = true;
-//       }
-//     });
-//     if (!sameProduct) {
-//       this.setState((prevState) => ({
-//         cartProduct: [...prevState.cartProduct,
-//           products.find((product) => product.id === idProduct)],
-//       }
-//       ));
-//     }
-//   };
+  /*     addProductCart = (idProduct) => {
+      const { products, cartProduct } = this.state;
+      let sameProduct = false;
+      cartProduct.forEach((product) => {
+        if (product.id === idProduct) {
+          if (product.quantity) {
+            product.quantity += 1;
+          } else {
+            product.quantity = 2;
+          }
+          sameProduct = true;
+        }
+      });
+      if (!sameProduct) {
+        this.setState((prevState) => ({
+          cartProduct: [...prevState.cartProduct,
+            products.find((product) => product.id === idProduct)],
+        }
+        ));
+      }
+    }; */
 
   handleClick = async (category = '', query = '') => {
     const response = await getProductsFromCategoryAndQuery(category, query);
