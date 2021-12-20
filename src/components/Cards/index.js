55bae0b4b5eb37e0668805e083d8560d7f3908a5
Emerson <eemr3@yahoo.com.br>
2021-12-20 17:52:11 -0300

    feat(Requisito - 11): trabalhando no requisto 11

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -13,10 +13,6 @@ class Cards extends Component {
           <ProdutctCard
             key={ product.id }
             addProductCart={ addProductCart }
-            // title={ product.title }
-            // price={ product.price }
-            // thumbnail={ product.thumbnail }
-            // idProduct={ product.id }
             product={ product }
           />
         )) }
