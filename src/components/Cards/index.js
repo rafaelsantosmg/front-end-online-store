a451ef4461c81ce4137d0924727be53a10befc8c
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-02-20 14:35:56 -0300

    feat: implemntado spinner

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Spinner } from 'react-bootstrap';
 import ProdutctCard from '../ProdutctCard';
 
 import './Cards.css';
@@ -9,29 +10,38 @@ class Cards extends Component {
     const { products, addProductCart } = this.props;
     return (
       <div className="cards-contaniner">
-        { products.map((product) => (
-          <ProdutctCard
-            key={ product.id }
-            addProductCart={ addProductCart }
-            product={ product }
+        {products.length > 0 ? (
+          products.map((product) => (
+            <ProdutctCard
+              key={ product.id }
+              addProductCart={ addProductCart }
+              product={ product }
+            />
+          ))
+        ) : (
+          <Spinner
+            animation="border"
+            variant="warning"
+            style={ { width: '10rem', height: '10rem' } }
           />
-        )) }
+        )}
       </div>
-
     );
   }
 }
 
 Cards.propTypes = {
   addProductCart: PropTypes.func,
-  products: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string,
-    price: PropTypes.number,
-  })),
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      price: PropTypes.number,
+    }),
+  ),
 };
 
 Cards.defaultProps = {
-  addProductCart: () => { },
+  addProductCart: () => {},
   products: [],
 };
 
